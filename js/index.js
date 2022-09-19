a1bd6dfd310fc332baaf731ceb450b60ce010d47
andriibeha <beha.andrii@gmail.com>
2022-09-19 15:47:18 +0300

    add fetch for click button

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ import { startBtnHandler } from "./screenOne.js";
 import { dressListHandler } from "./screenTwo.js";
 import { okBtnHandler } from "./screenThree.js";
 
-const startBtn = document.querySelector("#startBtn");
+const startBtnClick = document.querySelector("#startBtn-click");
 const dressList = document.querySelector("#dressList");
 const okBtn = document.querySelector("#okBtn");
 
 
 
 //Screen One
-startBtn.addEventListener("click", startBtnHandler);
+startBtnClick.addEventListener("click", startBtnHandler);
 
 //Screen Two
 dressList.addEventListener("click", dressListHandler);
--- js/screenOne.js
+++ js/screenOne.js
@@ -1,6 +1,7 @@
 import { musicPlay } from "./musicPlay.js";
 
 const startBtn = document.querySelector("#startBtn");
+const startBtnClick = document.querySelector("#startBtn-click");
 const bubbleOne = document.querySelector("#bubbleOne");
 const cursorOne = document.querySelector("#cursor-one");
 const screenTwo = document.querySelector("#screen-two");
@@ -13,6 +14,7 @@ export function startBtnHandler(e) {
     bubbleOne.style.display = "none";
     startBtn.style.display = "none";
     cursorOne.style.display = "none";
+    startBtnClick.style.display = "none";
     screenTwo.style.display = "block";
 
     startBtn.removeEventListener("click", startBtnHandler)
